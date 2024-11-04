30bcccc7ec9b9a7ef1794236b1092f5c42b1103d
zaydam1000000 <86502397+zayndamji@users.noreply.github.com>
2024-11-03 21:55:44 -0800

    added loading in from local storage

--- script.js
+++ script.js
@@ -3,8 +3,32 @@ const notesList = document.getElementById('notesList');
 
 const notes = {};
 
+const uuidList = JSON.parse(localStorage.getItem('uuidList'));
+if (uuidList) {
+  for (const uuid of uuidList) {
+    appendNoteToGUI(uuid, localStorage[uuid]);
+  }
+}
+
 createNote.addEventListener('click', () => {
-  const uuid = crypto.randomUUID();
+  appendNoteToGUI(crypto.randomUUID());
+});
+
+function appendNoteToGUI(uuid, content) {
+  console.log(uuid, content);
+
+  if (content == undefined) {
+    content = "";
+  }
+
+  const uuidList = JSON.parse(localStorage.getItem('uuidList'));
+  if (uuidList && !uuidList.includes(uuid)) {
+    uuidList.push(uuid);
+    localStorage.setItem('uuidList', JSON.stringify(uuidList));
+  } else {
+    localStorage.setItem('uuidList', JSON.stringify([uuid]));
+  }
+  localStorage[uuid] = '';
 
   const note = document.createElement('div');
   note.classList.add('note');
@@ -21,4 +45,6 @@ createNote.addEventListener('click', () => {
   notes[uuid].codemirror.on('change', () => {
     localStorage[uuid] = notes[uuid].value();
   });
-});
+
+  notes[uuid].value(content);
+}
