56234bcab258d2f545c11b855545d9fab30c432d
zaydam1000000 <86502397+zayndamji@users.noreply.github.com>
2024-11-03 21:40:54 -0800

    added markdown editor and saving to local storage

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 const createNote = document.getElementById('createNote');
 const notesList = document.getElementById('notesList');
 
+const notes = {};
+
 createNote.addEventListener('click', () => {
+  const uuid = crypto.randomUUID();
+
   const note = document.createElement('div');
   note.classList.add('note');
-  note.setAttribute('uuid', crypto.randomUUID());
+  note.setAttribute('uuid', uuid);
 
-  const noteInput = document.createElement('div');
-  noteInput.classList.add('noteInput');
-  noteInput.contentEditable = true;
+  const noteInput = document.createElement('textarea');
 
   note.append(noteInput);
 
   notesList.append(note);
+
+  notes[uuid] = new SimpleMDE({ element: noteInput });
+
+  notes[uuid].codemirror.on('change', () => {
+    localStorage[uuid] = notes[uuid].value();
+  });
 });
